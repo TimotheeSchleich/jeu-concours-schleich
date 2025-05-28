855533dff502881b77f34cc1032012dc420581bb
TimotheeSchleich <timothee.pobel@schleich-s.com>
2025-05-28 14:06:46 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,45 @@
+
+from flask import Flask, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    with open('index.html', 'r', encoding='utf-8') as f:
+        return f.read()
+
+@app.route('/modalites')
+def modalites():
+    with open('modalites.html', 'r', encoding='utf-8') as f:
+        return f.read()
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = [
+        request.form['ville'],
+        request.form['nom'],
+        request.form['email'],
+        request.form['telephone']
+    ]
+    with open('participants.csv', 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+    return redirect('/merci')
+
+@app.route('/merci')
+def merci():
+    return "<h1>Merci pour votre participation ! 🦍</h1>"
+
+@app.route('/tirage')
+def tirage():
+    import random
+    with open('participants.csv', encoding='utf-8') as f:
+        participants = list(csv.reader(f))
+    if not participants:
+        return "<h1>Aucune participation enregistrée</h1>"
+    gagnant = random.choice(participants)
+    return f"<h1>Le gagnant est : {gagnant[1]} de {gagnant[0]} !</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
